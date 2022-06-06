b3989dc21a5c9ee4183d01b55d8bf7d2d956b44c
Ivascu Robert <robert.ivascu@infoworld.ro>
2022-06-07 00:19:05 +0300

    HOME PAGE - 1. Header + poza/film de prezentare

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Observable, of } from 'rxjs';
-import { Product } from 'src/app/shared/class/base-cart-item';
 
 @Component({
   selector: 'app-header',
@@ -9,24 +7,29 @@ import { Product } from 'src/app/shared/class/base-cart-item';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
   @Output() openSideNavDrawer = new EventEmitter<boolean>();
 
-  cart$: Observable<Product>;
-  isOpen = false;
+  isSerachOverlayOpened = false;
+  isMenuOverlayOpened = false;
+  azetDtLocation = 'https://www.google.com/maps/place/Azet+DT+Resapare+Anvelope+Camion+SRL/@45.6575573,25.641716,17z/data=!3m1!4b1!4m12!1m6!3m5!1s0x40b35c6b2de4023d:0x1b5da9eebf44f902!2sAzet+DT+Resapare+Anvelope+Camion+SRL!8m2!3d45.6575587!4d25.6439095!3m4!1s0x40b35c6b2de4023d:0x1b5da9eebf44f902!8m2!3d45.6575587!4d25.6439095';
 
-  constructor(
-    // public cartService: CartService<Product>,
-    public dialog: MatDialog
-  ) {
-    // this.cart$ = this.cartService.cartProducts$;
-    this.cart$ = of();
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void { }
+
+  search(text: string): void {
+    console.log(text, 'text');
   }
 
-  ngOnInit(): void {
+  toggleSearchOverlay(): void {
+    this.isSerachOverlayOpened = !this.isSerachOverlayOpened;
+    this.isMenuOverlayOpened = false;
   }
 
-  removeCartProduct(product: Product): void {
-    // this.cartService.removeCartProduct(product);
+  toggleMenuOverlay(): void {
+    this.isMenuOverlayOpened = !this.isMenuOverlayOpened;
+    this.isSerachOverlayOpened = false;
   }
 
   clickMenuIcon(): void {
