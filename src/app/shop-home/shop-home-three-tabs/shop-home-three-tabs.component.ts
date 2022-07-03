91f6b5d6be4b6b5a2ba54a1642652de00fdfa150
Ivascu Robert <robert.ivascu@infoworld.ro>
2022-07-03 14:11:26 +0300

    HOME PAGE - 2. Taburi cu 3 elemente

--- src/app/shop-home/shop-home-three-tabs/shop-home-three-tabs.component.ts
+++ src/app/shop-home/shop-home-three-tabs/shop-home-three-tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { DropdownValue } from 'src/app/shared/interface/dropdown-value';
 
 @Component({
@@ -8,75 +9,117 @@ import { DropdownValue } from 'src/app/shared/interface/dropdown-value';
 })
 export class ShopHomeThreeTabsComponent implements OnInit {
 
+  formGroup = this.formBuilder.group({
+    width: '205',
+    height: '55',
+    diameter: 'R16',
+    vehicleType: '',
+    season: '',
+    speedIndex: '91V',
+    manufacturer: '',
+  });
+
+  get width(): FormControl {
+    return this.formGroup.get('width') as FormControl;
+  }
+
+  get height(): FormControl {
+    return this.formGroup.get('height') as FormControl;
+  }
+
+  get diameter(): FormControl {
+    return this.formGroup.get('diameter') as FormControl;
+  }
+
+  get speedIndex(): FormControl {
+    return this.formGroup.get('speedIndex') as FormControl;
+  }
+
+
   tireListByWeather: DropdownValue[] = [
     {
       label: 'Winter',
       value: 'Winter',
-      imageUrl: ''
+      imageUrl: '',
+      routerLink: ''
     },
     {
       label: 'Winter',
       value: 'Winter',
-      imageUrl: ''
+      imageUrl: '',
+      routerLink: ''
     },
     {
       label: 'Winter',
       value: 'Winter',
-      imageUrl: ''
+      imageUrl: '',
+      routerLink: ''
     },
     {
       label: 'Winter',
       value: 'Winter',
-      imageUrl: ''
+      imageUrl: '',
+      routerLink: ''
     },
     {
       label: 'Winter',
       value: 'Winter',
-      imageUrl: ''
+      imageUrl: '',
+      routerLink: ''
     },
     {
       label: 'Winter',
       value: 'Winter',
-      imageUrl: ''
+      imageUrl: '',
+      routerLink: ''
     },
   ];
 
   tireListByType: DropdownValue[] = [
     {
-      label: 'Observe',
-      value: 'Observe',
-      imageUrl: ''
+      label: 'SPORTS CARS',
+      value: 'SPORTS CARS',
+      imageUrl: '',
+      routerLink: ''
     },
     {
-      label: 'Observe',
-      value: 'Observe',
-      imageUrl: ''
+      label: 'SUVS',
+      value: 'SUVS',
+      imageUrl: '',
+      routerLink: ''
     },
     {
-      label: 'Observe',
-      value: 'Observe',
-      imageUrl: ''
+      label: 'ORIGINAL EQUIPTMENT',
+      value: 'ORIGINAL EQUIPTMENT',
+      imageUrl: '',
+      routerLink: ''
     },
     {
-      label: 'Observe',
-      value: 'Observe',
-      imageUrl: ''
+      label: '4X4 VEHICLES',
+      value: '4X4 VEHICLES',
+      imageUrl: '',
+      routerLink: ''
     },
     {
-      label: 'Observe',
-      value: 'Observe',
-      imageUrl: ''
+      label: 'TRACK CARS',
+      value: 'TRACK CARS',
+      imageUrl: '',
+      routerLink: ''
     },
     {
-      label: 'Observe',
-      value: 'Observe',
-      imageUrl: ''
+      label: 'VANS',
+      value: 'VANS',
+      imageUrl: '',
+      routerLink: ''
     },
   ];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log(this.formGroup.getRawValue(), 'e')
+  }
 }
