ec043a0de00da297e08a643064d2172d821a5114
Robert I <robertivascu24@gmail.com>
2023-07-23 10:40:01 +0300

    wip

--- src/app/shared/pipes/currency.pipe.ts
+++ src/app/shared/pipes/currency.pipe.ts
@@ -3,17 +3,20 @@ import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'azetDtCurrency' })
 export class AzetDTCurrencyPipe implements PipeTransform {
-
-  constructor(@Inject(LOCALE_ID) private locale: string) {
-  }
+  constructor(@Inject(LOCALE_ID) private locale: string) {}
 
   transform(
     value: number | null,
+    tva?: boolean,
     currencyCode: string = 'lei ',
     display: 'code' | 'symbol' | 'symbol-narrow' | string | boolean = 'symbol',
     digitsInfo: string = '1.2-2'
   ): string | null {
     if (value != null) {
+      if (tva) {
+        value = value - (value / 100) * 19;
+      }
+
       return formatCurrency(
         value,
         this.locale,
